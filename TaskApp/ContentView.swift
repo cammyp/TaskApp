1808c1debe205230ca1911f5d1393460ba6184f3
Cameron Partee <cp3082@nyu.edu>
2020-01-02 17:15:05 -0500

    Inital project commit

---
+++ TaskApp/ContentView.swift
@@ -0,0 +1,16 @@
+//  Created by Cameron  Partee on 1/2/20.
+//  Copyright © 2020 Cameron Partee. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
